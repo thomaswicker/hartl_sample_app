014884d125f5226ae3df78b95c53a34264d2cfdf
Thomas Wicker <design@thomaswicker.com>
2015-02-26 22:25:25 -0700

    finalizing layouts

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial App"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial App"
+  end
+
+  test "should get login page" do
+    get :signup
+    assert_response :success
+    assert_select "sign up", "Contact | Ruby on Rails Tutorial App"
+  end
 end
