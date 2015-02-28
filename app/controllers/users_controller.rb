f748ddc07f621eaa82490335432186c5182b1439
Thomas Wicker <design@thomaswicker.com>
2015-02-28 13:12:00 -0700

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     # finding the params on the user passed in and getting the :id. The @user variablele is then passed to the view.
     @user = User.find(params[:id])
-    # can the 'debugger' line to fire off a terminal debug mode
+    # can the 'debugger' line to fire off a terminal debug mode 
   end
 
   def new
