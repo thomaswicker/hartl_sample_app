f868ecff33709188b09883a51aa2765955bfa22e
Thomas Wicker <design@thomaswicker.com>
2015-02-28 13:07:53 -0700

    testing ssh key

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     # finding the params on the user passed in and getting the :id. The @user variablele is then passed to the view.
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) 
     # can the 'debugger' line to fire off a terminal debug mode
   end
 
