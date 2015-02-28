dba214c8ddbe7162489ebbd8556c038a54cca006
Thomas Wicker <design@thomaswicker.com>
2015-02-28 12:59:08 -0700

    adding is Sign Up flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  def show
+    # finding the params on the user passed in and getting the :id. The @user variablele is then passed to the view.
+    @user = User.find(params[:id])
+    # can the 'debugger' line to fire off a terminal debug mode
+  end
+
   def new
+    @user = User.new
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+		# Returns the Gravatar for a given user.
+
+		def gravatar_for(user)
+			# Digest::MD5::hexdigest(user.email) encrpts the email passed in and returns as a hexidecimal string, then pass that to the interpolated gravatar_id in the gravatar_url, then pass that whole object into the image_tag for final output to the view. We downcase the user.email because the MD5 hash result is case senstive so USER@email.com and user@email.com return different users.
+			gravatar_id = Digest::MD5::hexdigest(user.email.downcase) 
+			gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+			image_tag(gravatar_url, alt: user.name, class: "gravatar")
+		end
 end
