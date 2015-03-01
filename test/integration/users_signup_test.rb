f9c5372d8243ec4bf727a68ad80fb89d81dfc2ff
Thomas Wicker <design@thomaswicker.com>
2015-03-01 09:43:36 -0700

    testing

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get signup_path
+
+	test "invalid sign up information" do
+  	get signup_path
+  	before_count = User.count
+  	post users_path, user: {name: "", email: "thomas@thomas.com", password: "pass", password_confirmation: "pass" }
+  	assert_tempalte 'users/new'
+	end
 end
